f6e57a1a55ba4193d16ebe81a99cfde5d3272224
VladislavShV <shundrovvladislav@gmail.com>
2019-10-07 18:11:55 +0300

    first

---
+++ babbling/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var add = function add(a, b) {
+    return a + b;
+};
+
+var arrowFunction = function arrowFunction() {
+    return "Hello World";
+};
+
+alert(arrowFunction());
---
+++ babbling/src/main.js
@@ -0,0 +1,9 @@
+let add = (a,b) => {
+    return a+b;
+ }
+
+var arrowFunction = () => {
+    return "Hello World";
+};
+
+alert(arrowFunction());
